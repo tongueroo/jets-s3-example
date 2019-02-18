0ba640d4ea5b4638fa81c855d6682bf1e2e11c2e
Tung Nguyen <tongueroo@gmail.com>
2019-02-18 02:18:52 +0000

    update s3_job.rb

--- app/jobs/s3_job.rb
+++ app/jobs/s3_job.rb
@@ -1,13 +1,17 @@
 class S3Job < ApplicationJob
   s3_event ENV["S3_BUCKET"]
   def process
-    puts "process event #{JSON.dump(event)}"
+    puts "event #{JSON.dump(event)}"
+    puts "s3_event #{JSON.dump(s3_event)}"
+    puts "s3_object #{JSON.dump(s3_object)}"
     # Your logic
   end
 
   s3_event ENV["S3_BUCKET"]
   def resize
-    puts "resize event #{JSON.dump(event)}"
+    puts "event #{JSON.dump(event)}"
+    puts "s3_event #{JSON.dump(s3_event)}"
+    puts "s3_object #{JSON.dump(s3_object)}"
     # Your logic
   end
 end
